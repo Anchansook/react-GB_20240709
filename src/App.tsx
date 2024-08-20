a46c5b4f66f4c772bcb1674b3b94661b83946e75
ckstnr99 <ckstnr99@naver.com>
2024-08-20 09:54:40 +0900

    2024.08.20. 09:54

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 // import Component, { ClassComponent, FunctonComponent } from './component/Component';
 import CurlyBraces from './component/CurlyBraces';
+import Properties from './component_manage/Properties';
+
+//# npm run start 할 시에는 package.json 파일이 있는 자리에서 해야 함!
 
 function App() {
   return (
@@ -10,7 +13,9 @@ function App() {
       {/* <ClassComponent /> */}
       {/* <FunctonComponent /> */}
 
-      <CurlyBraces />
+      {/* <CurlyBraces /> */}
+
+      <Properties />
     </>
   );
 }
