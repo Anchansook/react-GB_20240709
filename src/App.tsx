38f29dd5256adc17ff09351da0921b1d8a5899f8
ckstnr99 <ckstnr99@naver.com>
2024-08-20 11:54:56 +0900

    2024.08.20. 11:54

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import './App.css';
 // import Component, { ClassComponent, FunctonComponent } from './component/Component';
 // import CurlyBraces from './component/CurlyBraces';
-import Properties from './component_manage/Properties';
+// import Properties from './component_manage/Properties';
+// import Gallery from './component_manage/example/Example1';
+import ConditionalRender from './component_manage/ConditionalRender';
 
 //# npm run start 할 시에는 package.json 파일이 있는 자리에서 해야 함!
 
 function App() {
   return (
     <>
+      {/* === component === */}
       {/* <Component /> */}
       {/* <ClassComponent /> */}
       {/* <FunctonComponent /> */}
 
       {/* <CurlyBraces /> */}
 
-      <Properties />
+      {/* === component_manage === */}
+      {/* <Properties /> */}
+      {/* <Gallery /> */}
+      <ConditionalRender />
     </>
   );
 }
